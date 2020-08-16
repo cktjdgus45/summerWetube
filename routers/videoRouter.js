3f1f8bb7caada8f812e6bfab0647b507b7241d5a
cktjdgus45 <cktjdgus45@naver.com>
2020-08-16 20:04:36 +0900

    MVC part 2,3

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,19 @@
 import express from "express";
+import routes from "../routers/routes";
+import {
+  videos,
+  upload,
+  videoDetail,
+  editVideo,
+  deleteVideo,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
+
+videoRouter.get("/", videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.editVideo, editVideo);
+videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.videoDetail, videoDetail);
 
 export default videoRouter;
