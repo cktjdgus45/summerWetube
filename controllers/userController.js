1d3e8200508b3c7667b4151e38b8118d781b266e
cktjdgus45 <cktjdgus45@naver.com>
2020-08-17 19:50:05 +0900

    Search Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,11 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const users = (req, res) => res.render("users");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const join = (req, res) => res.render("join", { pageTitle: "JOIN" });
+export const login = (req, res) => res.render("login", { pageTitle: "LOGIN" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "LOGOUT" });
+export const users = (req, res) => res.render("users", { pageTitle: "USERS" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "USER DETAIL" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "EDIT PROFILE" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "CHANGE PASSWORD" });
