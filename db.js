52cfe2090350631fc71c5cfb1de6681f49ff9538
cktjdgus45 <cktjdgus45@naver.com>
2020-08-20 10:51:20 +0900

    Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,41 +1,14 @@
-export const videos = [
-  {
-    id: 111111,
-    title: "video Title1",
-    description: "the video is about..",
-    views: 24,
-    videoFile:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-    creator: {
-      id: 121212,
-      name: "차성현",
-      email: "cktjdgus45@naver.com",
-    },
-  },
-  {
-    id: 222222,
-    title: "video Title2",
-    description: "the video is about..",
-    views: 24,
-    videoFile:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-    creator: {
-      id: 121212,
-      name: "차성현",
-      email: "cktjdgus45@naver.com",
-    },
-  },
-  {
-    id: 333333,
-    title: "video Title3",
-    description: "the video is about..",
-    views: 24,
-    videoFile:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-    creator: {
-      id: 121212,
-      name: "차성현",
-      email: "cktjdgus45@naver.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/we-tube", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("connect success");
+const handleError = () => console.error.bind(console, "connection error");
+
+db.once("open", handleOpen);
+db.on("error", handleError);
